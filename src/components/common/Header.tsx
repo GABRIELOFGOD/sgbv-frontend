46370cb2f7d5247c3155096a8954e23b66d3e3a6
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2025-02-27 20:01:45 +0100

    GREAT IS THY FAITHFULNESS LLORD  UNTO ME

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -16,6 +16,7 @@ import { usePathname } from "next/navigation";
 const Header = () => {
   const { toggleTheme, theme } = useTheme();
   const [openDropDown, setOpenDropDown] = useState<boolean>(false);
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
 
   const path = usePathname();
   
@@ -29,12 +30,13 @@ const Header = () => {
           width={50}
         />
       </Link>
-      <div className="hidden md:flex gap-5 my-auto">
+      <div className={`md:flex absolute shadow-md md:shadow-none md:relative w-full md:w-fit py-10 md:py-0 bg-[#f8f6f5] left-0 md:left-auto top-20 duration-200 md:top-auto gap-5 my-auto ${menuOpen ? "flex flex-col" : "hidden"}`}>
         {menuItem.map((item, index) => (
           <div key={index} className="my-auto text-lg">
             {item.link ? (
               <Link
-                className={`hover:font-[500] duration-200 ${path === item.link ? "font-bold" : ""}`}
+                onClick={() => setMenuOpen(false)}
+                className={`hover:font-[500] px-4 md:px-0 duration-200 ${path === item.link ? "font-bold" : ""}`}
                 href={item.link}
               >{item.name}</Link>
             ) : (
@@ -43,11 +45,12 @@ const Header = () => {
                 onMouseEnter={() => setOpenDropDown(true)}
                 onMouseLeave={() => setOpenDropDown(false)}
               >
-                <span className="cursor-pointer flex my-auto gap-2">{item.name} <RiArrowDropDownLine className="my-auto" size={20} /></span>
-                <div className={`absolute flex-col bg-[#f8f6f5] shadow-sm py-5 mt-8 border-grey-500 flex gap-5 text-sm rounded-md ${openDropDown ? "flex" : "hidden"}`}>
+                <span className="cursor-pointer flex my-auto gap-2 px-4 md:px-0">{item.name} <RiArrowDropDownLine className="my-auto" size={20} /></span>
+                <div className={`md:absolute flex-col w-full md:w-fit bg-[#f8f6f5] md:shadow-sm py-5 md:mt-8 border-grey-500 flex gap-5 text-sm rounded-md ${openDropDown ? "flex" : "hidden"}`}>
                   {item.subMenu?.map((subItem, index) => (
                     <Link
                       href={subItem.link!}
+                      onClick={() => setMenuOpen(false)}
                       key={index}
                       className={`hover:font-[500] duration-200 text-nowrap px-5 py-2 hover:bg-[#2a66ec] z-40 ${path === subItem.link ? "font-bold bg-[#2a66ec]" : ""}`}
                     >{subItem.name}</Link>
@@ -71,7 +74,10 @@ const Header = () => {
                   </span>
                 </button>
               </div>
-              <button className="my-auto cursor-pointer flex md:hidden">
+              <button 
+                className="my-auto cursor-pointer flex md:hidden"
+                onClick={() => setMenuOpen(!menuOpen)}
+              >
                 <BiMenu size={30} className="my-auto" color="black" />
               </button>
               <Button
